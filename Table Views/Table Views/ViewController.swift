cd4026b5ab864b4b407bb5875b903acd21a98b97
Naing Lin Aung <nainglinaung91@gmail.com>
2015-02-26 11:06:27 +0630

    test 3 on Table 3

---
+++ Table Views/Table Views/ViewController.swift
@@ -0,0 +1,37 @@
+//
+//  ViewController.swift
+//  Table Views
+//
+//  Created by Naing Lin Aung on 2/26/15.
+//  Copyright (c) 2015 Naing Lin Aung. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UITableViewDelegate{
+ 
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: UITableViewCellStyle.Default, reuseIdentifier: "Cell")
+        cell.textLabel?.text = "test"
+        return cell
+    }
+    
+    
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
