3e927d7c0c4d24524fcf06aa1a2ac06d7f9fb1a7
Naing Lin Aung <nainglinaung91@gmail.com>
2015-02-26 11:10:24 +0630

    with dynamic data

--- Table Views/Table Views/ViewController.swift
+++ Table Views/Table Views/ViewController.swift
@@ -10,18 +10,21 @@ import UIKit
 
 class ViewController: UIViewController, UITableViewDelegate{
  
+    
+    var cellContent = ["Naing Lin Aung","Okkar", "Arkar","Thet Paing OO","Ye Thu Aung"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 3
+        return cellContent.count
     }
     
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let cell = UITableViewCell(style: UITableViewCellStyle.Default, reuseIdentifier: "Cell")
-        cell.textLabel?.text = "test"
+        cell.textLabel?.text = cellContent[indexPath.row]
         return cell
     }
     
