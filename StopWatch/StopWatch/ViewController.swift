aa82b2f1c6124247d0c3e1d1a5b7a47709d295bb
Naing Lin Aung <nainglinaung91@gmail.com>
2015-02-26 10:01:37 +0630

    stopWatch better fix

--- StopWatch/StopWatch/ViewController.swift
+++ StopWatch/StopWatch/ViewController.swift
@@ -12,21 +12,16 @@ class ViewController: UIViewController {
     
     var timer = NSTimer()
     @IBOutlet var time: UILabel!
-    var count = 0
-    var sec = 0
-    var min = 0
-    var hour = 0
+    var count = 0, sec = 0, min = 0, hour = 0
     
     func updateTime() {
         count++
-        var seconds:String, minutes:String,hours:String
         var milisec = 0
         
         if count > 99 {
             sec++
             count -= 100
         }
-        
         
         if sec > 59 {
             min++
@@ -39,30 +34,28 @@ class ViewController: UIViewController {
             min -= 60
         }
         
-        minutes = (min < 10) ? "0\(min)" : "\(min)"
-        seconds = (sec < 10) ? "0\(sec)" : "\(sec)"
-        hours   = (hour < 10) ? "0\(hour)" : "\(hour)"
-        
+        var minutes = (min < 10) ? "0\(min)" : "\(min)"
+        var seconds = (sec < 10) ? "0\(sec)" : "\(sec)"
+        var hours   = (hour < 10) ? "0\(hour)" : "\(hour)"
         
         time.text = "\(hours):\(minutes):\(seconds):\(count)"
-        //println("\(count)")
+
     }
     
     
     @IBAction func play(sender: AnyObject) {
-    
         timer = NSTimer.scheduledTimerWithTimeInterval(0.01, target: self, selector: Selector("updateTime"), userInfo: nil, repeats: true)
     
     }
+    
     @IBAction func stop(sender: AnyObject) {
         timer.invalidate()
         count = 0
         time.text = "00:00:00:00"
     }
+    
     @IBAction func pause(sender: AnyObject) {
         timer.invalidate()
-    
-        
     }
 
     override func viewDidLoad() {
