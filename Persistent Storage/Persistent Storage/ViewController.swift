bd94757e1c0ef8298fd14a7899aa6afa22732888
Naing Lin Aung <nainglinaung91@gmail.com>
2015-02-27 13:10:42 +0630

    Persistant Storage

---
+++ Persistent Storage/Persistent Storage/ViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  Persistent Storage
+//
+//  Created by Naing Lin Aung on 2/27/15.
+//  Copyright (c) 2015 Naing Lin Aung. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        var arr = ["lorem","ipsum","dollar"]
+        
+        NSUserDefaults.standardUserDefaults().setObject(arr, forKey: "get")
+        let get = NSUserDefaults.standardUserDefaults().objectForKey("get")? as NSArray
+        
+        println(get)
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
