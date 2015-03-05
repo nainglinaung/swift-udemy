21d8573599b3a70e8b1e315e1f31b9c7fcc1f503
Naing Lin Aung <nainglinaung91@gmail.com>
2015-03-05 13:19:23 +0630

    Keyboard added

---
+++ Keyboard/Keyboard/ViewController.swift
@@ -0,0 +1,42 @@
+//
+//  ViewController.swift
+//  Keyboard
+//
+//  Created by Naing Lin Aung on 3/5/15.
+//  Copyright (c) 2015 Naing Lin Aung. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController,UITextFieldDelegate {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.textField.delegate = self
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func press(sender: UIButton) {
+        label.text = textField.text
+    }
+    
+    override func touchesBegan(touches: NSSet, withEvent event: UIEvent) {
+        self.view.endEditing(true)
+    }
+    
+    
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    
+    
+    @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var textField: UITextField!
+}
+
