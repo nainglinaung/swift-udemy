24177a48fc0d6fcc23eb4bc993f0eb08e0daba11
Naing Lin Aung <nainglinaung91@gmail.com>
2015-03-05 16:11:38 +0630

    added todolist

---
+++ todoList/todoList/FirstViewController.swift
@@ -0,0 +1,62 @@
+//
+//  FirstViewController.swift
+//  todoList
+//
+//  Created by Naing Lin Aung on 3/5/15.
+//  Copyright (c) 2015 Naing Lin Aung. All rights reserved.
+//
+
+import UIKit
+
+var itemArray = [String]()
+
+class FirstViewController: UIViewController,UITableViewDelegate {
+
+    @IBOutlet weak var toDolistTable: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    
+        if NSUserDefaults.standardUserDefaults().objectForKey("itemArray") != nil {
+            itemArray = NSUserDefaults.standardUserDefaults().objectForKey("itemArray") as [String]
+        }
+    }
+    
+    func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
+        
+        if editingStyle == UITableViewCellEditingStyle.Delete {
+            itemArray.removeAtIndex(indexPath.row)
+            NSUserDefaults.standardUserDefaults().setObject(itemArray, forKey: "itemArray")
+            toDolistTable.reloadData()
+        }
+        
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+        toDolistTable.reloadData()
+    }
+    
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemArray.count
+    }
+    
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
+        let cell = UITableViewCell(style: UITableViewCellStyle.Default, reuseIdentifier: "cell")
+        
+         cell.textLabel?.text = itemArray[indexPath.row]
+        
+        return cell
+    }
+
+
+}
+
--- todoList/todoList/SecondViewController.swift
+++ todoList/todoList/SecondViewController.swift
@@ -1,6 +1,6 @@
 //
-//  ViewController.swift
-//  Keyboard
+//  SecondViewController.swift
+//  todoList
 //
 //  Created by Naing Lin Aung on 3/5/15.
 //  Copyright (c) 2015 Naing Lin Aung. All rights reserved.
@@ -8,35 +8,35 @@
 
 import UIKit
 
-class ViewController: UIViewController,UITextFieldDelegate {
+class SecondViewController: UIViewController,UITextFieldDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.textField.delegate = self
-    } 
+        // Do any additional setup after loading the view, typically from a nib.
+    }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
-    @IBAction func press(sender: UIButton) {
-        label.text = textField.text
+    @IBOutlet weak var item: UITextField!
+    
+    @IBAction func addItem(sender: UIButton) {
+        itemArray.append(item.text)
+        NSUserDefaults.standardUserDefaults().setObject(itemArray, forKey: "itemArray")
+        item.text = ""
     }
     
     override func touchesBegan(touches: NSSet, withEvent event: UIEvent) {
-        self.view.endEditing(true)
+        self.view.endEditing(true);
     }
-    
     
     func textFieldShouldReturn(textField: UITextField) -> Bool {
-        textField.resignFirstResponder()
+        item.resignFirstResponder()
         return true
     }
-    
-    
-    
-    @IBOutlet weak var label: UILabel!
-    @IBOutlet weak var textField: UITextField!
+
+
 }
 
