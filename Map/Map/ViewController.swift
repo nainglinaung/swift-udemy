a5b8892fb35627377dc29b99e699be1f75062bb6
Naing Lin Aung <nainglinaung91@gmail.com>
2015-03-17 14:54:25 +0630

    finish

--- Map/Map/ViewController.swift
+++ Map/Map/ViewController.swift
@@ -11,8 +11,23 @@ import MapKit
 
 class ViewController: UIViewController {
 
+   // 40.748570, -73.985696
+    @IBOutlet weak var mapView: MKMapView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        var longtitude:CLLocationDegrees = -73.985696
+        var latitude:CLLocationDegrees = 40.748570
+        var latDelta:CLLocationDegrees = 0.01
+        var longDelta:CLLocationDegrees = 0.01
+        
+        var span:MKCoordinateSpan = MKCoordinateSpanMake(latDelta, longDelta)
+        var location:CLLocationCoordinate2D = CLLocationCoordinate2DMake(latitude, longtitude)
+        
+        var region:MKCoordinateRegion = MKCoordinateRegionMake(location, span)
+        
+        mapView.setRegion(region, animated: true)
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -24,3 +39,4 @@ class ViewController: UIViewController {
 
 }
 
+ 
