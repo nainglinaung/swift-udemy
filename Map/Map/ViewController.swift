11ebf285d70c2acf527684ace38fcce60e03af31
Naing Lin Aung <nainglinaung91@gmail.com>
2015-03-17 14:09:24 +0630

    added map

---
+++ Map/Map/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Map
+//
+//  Created by Naing Lin Aung on 3/17/15.
+//  Copyright (c) 2015 Naing Lin Aung. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
