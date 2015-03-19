bea0e359527d98ff730d550d6af1484b8f5b271f
Naing Lin Aung <nainglinaung91@gmail.com>
2015-03-19 17:42:14 +0630

    checking CLGeoCoder

--- whereAmI/whereAmI/ViewController.swift
+++ whereAmI/whereAmI/ViewController.swift
@@ -7,19 +7,61 @@
 //
 
 import UIKit
+import CoreLocation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,CLLocationManagerDelegate {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-
+    @IBOutlet weak var location: UILabel!
     @IBOutlet weak var latitude: UILabel!
     @IBOutlet weak var longtitude: UILabel!
     @IBOutlet weak var course: UILabel!
     @IBOutlet weak var speed: UILabel!
     @IBOutlet weak var attitude: UILabel!
+    
+    
+    var manager:CLLocationManager?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        manager = CLLocationManager()
+        manager?.delegate = self
+        manager?.desiredAccuracy = kCLLocationAccuracyBest
+        manager?.requestWhenInUseAuthorization()
+        manager?.startUpdatingLocation()
+        
+    }
+    
+    
+    
+    func locationManager(manager: CLLocationManager!, didFailWithError error: NSError!) {
+        println(error)
+    }
+    
+    func locationManager(manager: CLLocationManager!, didUpdateLocations locations: [AnyObject]!) {
+        var userlocation = locations[0] as CLLocation
+        latitude.text    = "\(userlocation.coordinate.latitude)"
+        longtitude.text  = "\(userlocation.coordinate.longitude)"
+        attitude.text    = "\(userlocation.altitude)"
+        course.text      = "\(userlocation.course)"
+        speed.text       = "\(userlocation.speed)"
+        location.text    = ""
+       
+        CLGeocoder().reverseGeocodeLocation(userlocation, completionHandler: { (placeMarks, error) -> Void in
+            if error != nil {
+                println(error)
+                println("hi")
+            } else {
+                println(placeMarks)
+            }
+            
+            
+        })
+        
+        println(userlocation)
+    }
+    
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
