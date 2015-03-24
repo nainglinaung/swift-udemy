669ace8adda2c3f79616c0f726c830f8d3ea17bc
Naing Lin Aung <nainglinaung91@gmail.com>
2015-03-24 17:28:48 +0630

    downloading json finished

---
+++ working with JSON/working with JSON/ViewController.swift
@@ -0,0 +1,49 @@
+//
+//  ViewController.swift
+//  working with JSON
+//
+//  Created by Naing Lin Aung on 3/24/15.
+//  Copyright (c) 2015 Naing Lin Aung. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        let urlPath = "http://www.telize.com/geoip"
+        let url = NSURL(string: urlPath)
+        
+        let session = NSURLSession.sharedSession()
+        
+        let task = session.dataTaskWithURL(url!, completionHandler: { (data, response, error) -> Void in
+            if error != nil {
+                println(error)
+            } else {
+                println(data)
+                let jsonResult = NSJSONSerialization.JSONObjectWithData(data, options: NSJSONReadingOptions.MutableContainers, error: nil) as NSDictionary
+                
+                println(jsonResult)
+                
+            }
+            
+            
+        })
+        
+        task.resume()
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+    
+    
+}
+
