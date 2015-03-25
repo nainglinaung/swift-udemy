d67ffc3cb3a0f22f567eab335b47c2414de8f374
Naing Lin Aung <nainglinaung91@gmail.com>
2015-03-25 10:18:26 +0630

    Blog Reader start

---
+++ Blog Reader/Blog Reader/DetailViewController.swift
@@ -0,0 +1,43 @@
+//
+//  DetailViewController.swift
+//  Blog Reader
+//
+//  Created by Naing Lin Aung on 3/24/15.
+//  Copyright (c) 2015 Naing Lin Aung. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    @IBOutlet weak var detailDescriptionLabel: UILabel!
+
+
+    var detailItem: AnyObject? {
+        didSet {
+            // Update the view.
+            self.configureView()
+        }
+    }
+
+    func configureView() {
+        // Update the user interface for the detail item.
+            if let label = self.detailDescriptionLabel {
+                label.text = "HELLO WORLD"
+            }
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        self.configureView()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Blog Reader/Blog ReaderTests/Blog_ReaderTests.swift
@@ -0,0 +1,36 @@
+//
+//  Blog_ReaderTests.swift
+//  Blog ReaderTests
+//
+//  Created by Naing Lin Aung on 3/24/15.
+//  Copyright (c) 2015 Naing Lin Aung. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class Blog_ReaderTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
